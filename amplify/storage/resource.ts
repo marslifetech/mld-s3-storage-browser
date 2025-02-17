3156f0593a22f56b98f8f7f94bea00eb5a7b2924
Michael Uchytil <mjuchytil@marslife.dev>
2025-02-17 16:04:59 -0600

    updated-permissions

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,15 +1,15 @@
 import { defineStorage } from '@aws-amplify/backend';
 
 export const storage = defineStorage({
-  name: 'myS3Bucket',
+  name: 'myS356Bucket',
   access: (allow) => ({
     'public/*': [
       allow.guest.to(['read']),
-      allow.authenticated.to(['read', 'write', 'delete']),
+      allow.authenticated.to(['read', 'write']),
     ],
     'protected/{entity_id}/*': [
       allow.authenticated.to(['read']),
-      allow.entity('identity').to(['read', 'write', 'delete'])
+      allow.entity('identity').to(['read', 'write'])
     ],
     'private/{entity_id}/*': [
       allow.entity('identity').to(['read', 'write', 'delete'])
